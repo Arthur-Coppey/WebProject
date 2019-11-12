f7c719f13d0580bbdad067fef75f8bd50849d3e4
Arthur-Coppey <arthur.coppey@viacesi.fr>
2019-11-12 11:51:12 +0100

    updated models

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    /**
+     * overrides orm default table to fit
+     */
     protected $table = 'categories';
-    //
+
+    /**
+     *
+     *
+     */
+    protected $fillable = ['label'];
 }
