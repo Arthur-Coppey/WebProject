91dc421102645cc5747295f3c26c7ceca9fe2e7f
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2019-11-12 16:47:07 +0100

    adding seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,6 +10,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([]);
+        $this->call([
+            RolesTableSeeder::class,
+            CentersTableSeeder::class,
+            CategoriesTableSeeder::class,
+            UsersTableSeeder::class,
+            ]);
     }
 }
