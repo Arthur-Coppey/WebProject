31e1d4a086f6dca59765a05fa06aac4581cbf2b9
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2019-11-12 11:48:07 +0100

    updated models

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'label', 'descrition', 'location', 'date', 'price', 'recurring', 'hidden',
+    ];
 }
--- app/Picture.php
+++ app/Picture.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
-    //
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'URI', 'hidden',
+    ];
 }
