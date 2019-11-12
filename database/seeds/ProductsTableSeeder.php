bb657481a03c66921a9e7f98b00d475bed815a52
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2019-11-12 19:01:37 +0100

    adding seeders

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::truncate();
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++){
+            Product::create([
+                'label' => $faker->lastName,
+                'description' => $faker->word,
+                'price' => '10.00',
+                'center_id' => rand(1, 12)
+            ]);
+        }
+    }
+}
