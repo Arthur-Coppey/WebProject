958d93629e8a6f02e7ffa383b400be30626f7cd8
Axel-Galand <axel.galand@viacesi.fr>
2019-11-17 17:56:07 +0100

    added the final parts to paypal paiment functionality

--- resources/views/panier.blade.php
+++ resources/views/panier.blade.php
@@ -107,7 +107,6 @@
                 </a>
             </td>
             <td class="txt-totaux total-article on-right" width="16%">
-
                 <a class="text-articles-panier prix-total-panier">
                     @php
                         $addPay = 0;
@@ -118,7 +117,6 @@
 
                     @endphp
                 </a>
-
             </td>
         </tr>
     </tbody>
@@ -137,6 +135,29 @@
             </button>
         </div>
         <div class="paypal-paiment">
+            <script>
+                paypal.Buttons({
+                    createOrder: function(data, actions) {
+                    // This function sets up the details of the transaction, including the amount and line item details.
+                    return actions.order.create({
+                        purchase_units: [{
+                        amount: {
+                            value: '0.01'
+                        }
+                        }]
+                    });
+                    },
+                    onApprove: function(data, actions) {
+                    // This function captures the funds from the transaction.
+                    return actions.order.capture().then(function(details) {
+                        // This function shows a transaction success message to your buyer.
+                        alert('Transaction completed by ' + details.payer.name.given_name);
+                    });
+                    }
+                }).render('#paypal-button-container');
+                //This function displays Smart Payment Buttons on your web page.
+            </script>
+
             <script src="https://www.paypal.com/sdk/js?client-id=AevGLXweHKzzybNQzKGfEJoTca0O3LBI0T7JdKqSEbX8nvzt3acVk-wRNKYTbm5xijHvW6whCIhBK7bl"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.</script>
 
             <div id="paypal-button-container"></div>
