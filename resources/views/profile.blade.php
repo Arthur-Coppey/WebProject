3965e88cd76200123cfda2736967ea74fe847cd0
Pierre.F <pierre.forques@viacesi.fr>
2019-11-17 01:35:45 +0100

    profil edit : event inscription now diplay

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -34,7 +34,9 @@
 <div class="profil-title-div">
     <h2 class="titre-infos-profil">Centre : <?php echo App\Center::where('id', App\User::where('id' , \Auth::user()->id)->first()->center_id)->first()->name; ?></h2>
 </div>
-
+<div class="profil-title-div">
+        <h2 class="titre-infos-profil">Status : <?php echo App\Role::where('id', App\User::where('id' , \Auth::user()->id)->first()->role_id)->first()->role ?></h2>
+    </div>
 <div class="log-out-div">
     <a class="log-out-text" href="{{ route('logout') }}"
         style="text-decoration: none; color: white;"
@@ -46,7 +48,24 @@
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         @csrf
-    </form>
+    </form><br>
+
+    <div class="profil-title-div">
+        <h1 class="titre-profil">Evenement inscrit</h1>
+    </div>
+
+    @php
+        $participant = App\Participant::all();
+    @endphp
+
+    @foreach ($participant as $i=>$value)
+    @php    
+        echo App\Event::where('id', $value->event_id)->get('label')[0]['label'];
+
+    @endphp
+    @endforeach
+
+
 @endguest
      
 @endsection
